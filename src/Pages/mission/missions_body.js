987802a0f7c1a4dfd5385881686d5e7ed4da01f0
Marc raphael <marcraphael979@gmail.com>
2023-01-30 16:34:46 +0100

    Pages/mission/missions_body: dispatch join and leave actions

--- src/Pages/mission/missions_body.js
+++ src/Pages/mission/missions_body.js
@@ -1,9 +1,29 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { FetchMissions } from '../../Redux/Missions/Missions';
+import {
+  FetchMissions, loadMissions, leaveMission, joinMission,
+} from '../../Redux/Missions/Missions';
 
 const MissionsBody = () => {
+  const dispatch = useDispatch();
+  const datas = useSelector((state) => state.MissionsReducer.missions);
+
+  const missions = Object.values(datas);
+
+  useEffect(() => {
+    dispatch(FetchMissions());
+  }, [dispatch]);
+
+  const handleJoin = (e) => {
+    // Check if the button text is Join Mission or Leave Mission
+    if (e.target.textContent === 'Join Mission') {
+      dispatch(joinMission(e.target.id));
+    } else {
+      dispatch(leaveMission(e.target.id));
+    }
+  };
+
   return (
     <div>
       <h1>Missions</h1>
