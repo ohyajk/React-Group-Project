189a7b6e126b55ea5811b962c9e038a627064c2d
Marc raphael <marcraphael979@gmail.com>
2023-01-30 16:18:22 +0100

    Pages/mission/missions_body: import mission fetcher

--- src/Pages/mission/missions_body.js
+++ src/Pages/mission/missions_body.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
+
+import { FetchMissions } from '../../Redux/Missions/Missions';
 
 const MissionsBody = () => {
   return (
