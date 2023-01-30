7a4b71297ddbdc124e0089fe4db694f6cd278248
Marc raphael <marcraphael979@gmail.com>
2023-01-30 22:54:44 +0100

    Pages/Missions: import and render the missions

--- src/Pages/Missions.jsx
+++ src/Pages/Missions.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import MissionsBody from './mission/missions_body';
+
 const Missions = () => {
   return (
-    <div>Missions</div>
+    <div className="container">
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description </th>
+            <th>Status</th>
+            <th>Join</th>
+          </tr>
+        </thead>
+        <MissionsBody />
+      </table>
+    </div>
   );
 };
 
