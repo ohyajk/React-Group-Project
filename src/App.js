0d58873044739ecdd2cd37375deaea24dd72621d
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-30 18:42:14 +0530

    Add Redux folder and store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
 import Rockets from './Pages/Rockets';
 import Missions from './Pages/Missions';
 import Profile from './Pages/Profile';
 import Navbar from './Components/Navbar';
+import store from './Redux/ConfigureStore';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Rockets />} />
-        <Route path="/missions" element={<Missions />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
