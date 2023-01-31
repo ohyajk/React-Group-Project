4bc0312ec924d3df75a7cc675ae31a3e477665d9
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-02-01 01:57:36 +0530

    Add Profile and Rockets Tests

--- src/Tests/Rocket/Rockets.test.js
+++ src/Tests/Rocket/Rockets.test.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../../Redux/ConfigureStore';
 import Rockets from '../../Pages/Rockets';
 
 describe('testing for rockets data', () => {
   it('test for component', () => {
-    const test2 = render(
+    const test1 = render(
       <Provider store={store}>
         <Rockets />
       </Provider>,
     );
-    expect(test2).toMatchSnapshot();
+    expect(test1).toMatchSnapshot();
+  });
+  test('Check if Rockets Component Renders properly', () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+    expect(screen.queryByText(/Status/)).toBeNull();
+    expect(screen.queryByText(/Description/)).toBeNull();
   });
 });
+
