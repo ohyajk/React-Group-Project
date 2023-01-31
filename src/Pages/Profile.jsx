a070f28702f21cab6e4578d87f4037c7a1d1b049
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-31 14:54:55 +0530

    Add eslint fixer

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,28 +3,29 @@ import { useSelector } from 'react-redux';
 import './Profile.css';
 
 const Profile = () => {
-
   const rockets = useSelector((state) => state.rockets);
 
   return (
     <>
       <h2 className="profile-heading">My Profile</h2>
       <div className="profile">
-      <div className="rockets">
-        <h2>My Rockets</h2>
-        <ul>
-          {rockets.map(rkt => { return(
-            rkt.reserved === true ? <li key={rkt.name} >{rkt.name}</li> : '' 
-          )})}
-        </ul>
+        <div className="rockets">
+          <h2>My Rockets</h2>
+          <ul>
+            {rockets.map((rkt) => {
+              return (
+                rkt.reserved === true ? <li key={rkt.name}>{rkt.name}</li> : ''
+              );
+            })}
+          </ul>
+        </div>
+        <div className="missions">
+          <h2>My Missions</h2>
+          <ul>
+            <li>Mission 1</li>
+          </ul>
+        </div>
       </div>
-      <div className="missions">
-        <h2>My Missions</h2>
-        <ul>
-          <li>Mission 1</li>
-        </ul>
-      </div>
-    </div>
     </>
   );
 };
--- src/Pages/Rockets.jsx
+++ src/Pages/Rockets.jsx
@@ -1,26 +1,27 @@
-import { useDispatch, useSelector } from "react-redux";
-import { reserveRocket, cancelReservation } from "../Redux/Rockets/Rockets";
+import { useDispatch, useSelector } from 'react-redux';
+import { reserveRocket, cancelReservation } from '../Redux/Rockets/Rockets';
 import './Rockets.css';
 
 const Rockets = () => {
-
   const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
   return (
     <div>
       <ul className="rocket-ul">
-        {rockets.map((rkt) => { return(
-          <li key={rkt.id} className="rocket-li">
-          <img className="rocket-img" src={rkt.flickr_images} alt={rkt.id} />
-          <div className="rocket-txt">
-            <h2>{rkt.name}</h2>
-            {rkt.reserved === true ? <span className="reserve">&bull; Reserved</span> : ''}
-            <p>{rkt.description}</p>
-            {rkt.reserved === false ? <button className="btn1" onClick={()=> {dispatch(reserveRocket(rkt.id))}} type="button">MAKE RESERVATION</button> : <button className="btn2" onClick={()=> {dispatch(cancelReservation(rkt.id))}} type="button">CANCEL RESERVATION</button>}
-            
-          </div>
-        </li>
-        )})}  
+        {rockets.map((rkt) => {
+          return (
+            <li key={rkt.id} className="rocket-li">
+              <img className="rocket-img" src={rkt.flickr_images} alt={rkt.id} />
+              <div className="rocket-txt">
+                <h2>{rkt.name}</h2>
+                {rkt.reserved === true ? <span className="reserve">&bull; Reserved</span> : ''}
+                <p>{rkt.description}</p>
+                {rkt.reserved === false ? <button className="btn1" onClick={() => { dispatch(reserveRocket(rkt.id)); }} type="button">MAKE RESERVATION</button> : <button className="btn2" onClick={() => { dispatch(cancelReservation(rkt.id)); }} type="button">CANCEL RESERVATION</button>}
+
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
