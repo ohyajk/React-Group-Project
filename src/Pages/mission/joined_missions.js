c11562c52110eaf8c73beeb9512505886136ae22
Marc raphael <marcraphael979@gmail.com>
2023-02-01 02:53:02 +0100

    Pages/mission/joined_missions: Update mission list if empty

--- src/Pages/mission/joined_missions.js
+++ src/Pages/mission/joined_missions.js
@@ -6,9 +6,14 @@ const JoinedMissions = () => {
   const data = useSelector((state) => state.missions.missions);
 
   return (
-    <div className="missions">
+    <div className="missions-list">
       <h2>Joined missions</h2>
       <ul className="missions-list__missions">
+        {!data.length && (
+        <li className="missions-list__mission text-disabled">
+          No mission reserved...
+        </li>
+        )}
         {data.filter((mission) => mission.joined).map((mission) => (
           <div key={mission.mission_id}>
             <li className="missions-list__mission">{mission.mission_name}</li>
